22fc919f26f6dae3fafcffe2c17bacc3871d5ed7
kin280 <74567462+kin280@users.noreply.github.com>
2024-01-19 21:44:22 +0700

    Add project files.

---
+++ docfile.csv/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string filePath = "quocqia.csv";
+
+        try
+        {
+            
+            string[] lines = File.ReadAllLines(filePath);
+
+            
+            foreach (var line in lines)
+            {
+               
+                string[] columns = line.Split(',');
+
+                
+                Console.WriteLine($"Country Code: {columns[4]}, Country Name: {columns[5]}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Đã xảy ra lỗi: {ex.Message}");
+        }
+    }
+}
